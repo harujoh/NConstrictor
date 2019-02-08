6fa9c8df6b539d25c2f26187a0024c0ac939d328
春条 <harujoh@users.noreply.github.com>
2019-02-08 17:22:33 +0900

    Update Sample.cs

--- Sample.cs
+++ Sample.cs
@@ -7,35 +7,28 @@ namespace NConstrictorSample
     {
         static void Main(string[] args)
         {
-            dynamic py = new PyDynamic(Python.Main);
+            Python.Initialize();
 
-            float[,] array =
+            PyArray<float> x = new[,]
             {
                 {0.0f,  1.0f,  2.0f,  3.0f},
                 {4.0f,  5.0f,  6.0f,  7.0f},
                 {8.0f,  9.0f, 10.0f, 11.0f}
             };
-            py.x = array;
-            Python.Print("x");
+            Python.Print(x);
 
+            x[2, 1] += 50.0f;
+            Python.Print(x);
 
-            PyArray<float> pyArray = py.x;
-            pyArray[2, 1] += 50.0f;
-            Python.Print("x");
+            x += 50.0f;
+            Python.Print(x);
 
+            x += new[] { 100.0f, 200.0f, 300.0f, 400.0f };
+            Python.Print(x);
 
-            py.x += 50.0f;
-            Python.Print("x");
-
-
-            py.x += new[] { 100.0f, 200.0f, 300.0f, 400.0f };
-            Python.Print("x");
-
-
-            PyArray<float[]> pyArrayBuffer = py.x;
+            PyArray<float[]> pyArrayBuffer = new PyArray<float[]>(x);
             pyArrayBuffer[1] = new[] { 10.0f, 20.0f, 30.0f, 40.0f };
-            Python.Print("x");
-
+            Python.Print(x);
 
             Console.Read();
         }
